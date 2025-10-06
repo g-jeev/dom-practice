0ff57129e7209dbd74dd6a9d9071f14df9215b09
g-jeev <guimbaorajeevmueller@gmail.com>
2025-10-06 12:35:47 +0800

    second commit

---
+++ js-file.js
@@ -0,0 +1,29 @@
+// Select elements
+const container = document.querySelector("#container");
+
+const redParagraph = document.createElement("p");
+redParagraph.textContent = "Hey i'm red!";
+redParagraph.style.color = "red";
+
+container.appendChild(redParagraph);
+
+const blueHeading = document.createElement('h3');
+blueHeading.textContent = "I'm blue!";
+blueHeading.style.color = "blue";
+
+container.appendChild(blueHeading);
+
+const pinkDiv = document.createElement('div');
+pinkDiv.style.backgroundColor = 'pink';
+pinkDiv.style.border = '1px, solid black';
+
+
+const innerH1 = document.createElement('h1');
+innerH1.textContent = "I'm in div";
+
+container.appendChild(innerH1);
+
+const innerP = document.createElement('p');
+innerP.textContent = "ME TOO!";
+
+container.appendChild(innerP);
